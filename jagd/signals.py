377407ac04bdd7631f399243750ae53877f7ce8c
sylwia_buczek <buczek@bots4you.de>
2023-11-13 21:56:54 +0100

    category_filter: refactoring

--- jagd/signals.py
+++
@@ -1,8 +0,0 @@
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from django.contrib.auth.models import User
-from jagd.models import U18M, U18W, UE50M, UE50W, BJM, BJW, UserProfile
-
-
-#@receiver(post_save, sender=User)
-#def create_boulder_entry(sender, instance, created, **kwargs):
