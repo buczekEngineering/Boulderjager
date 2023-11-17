86580f5726cbed5df4b2d63c7c5a92bdc81764ea
sylwia_buczek <buczek@bots4you.de>
2023-11-17 11:48:29 +0100

    added production setup

--- jagd/urls.py
+++ jagd/urls.py
@@ -15,3 +15,8 @@ urlpatterns = [
     path('signin', views.signin, name='signin'),
     path('signout', views.signout, name='signout'),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_URL)
